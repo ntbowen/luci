a09e5dcdb0e1201eee668154056e33b5098d9537
ntbowen <59873000+ntbowen@users.noreply.github.com>
2025-02-27 14:27:53 +0800

    Update wireless.js
    修改缺省SSID

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -2318,7 +2318,7 @@ return view.extend({
 			uci.add('wireless', 'wifi-iface', section_id);
 			uci.set('wireless', section_id, 'device', radioDev.getName());
 			uci.set('wireless', section_id, 'mode', 'ap');
-			uci.set('wireless', section_id, 'ssid', 'ImmortalWrt');
+			uci.set('wireless', section_id, 'ssid', 'ZagWIFI');
 			uci.set('wireless', section_id, 'encryption', 'none');
 
 			this.addedSection = section_id;
